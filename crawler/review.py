85c84d6940c08ba33ee6cf99001489c9ad81cde4
Alkacchi <66722150+Alkahestry@users.noreply.github.com>
2022-07-14 09:48:19 +0700

    Clean code

--- crawler/review.py
+++ crawler/review.py
@@ -21,31 +21,19 @@ class Review:
         return self.sentencizer.format_text(self.reviews['text'][index])
     
     def sentencizer_func(self, str : str) -> list:
-        print("Sentencizer", type(str))
         return self.sentencizer.sents(str)
     
     def summarizer_func(self, str) -> list:
         return self.summarizer.summarize(str)
     
     def filter_text(self, index : int, str : list) -> list:
-        print(f"Filter {type(str)}")
         return self.filter.filter_all(self.reviews['anime_uid'][index], str)
     
     def process(self, index : int):
         str = self.format_txt(index)
-        print(f"Return format_txt {type(str)}")
         str = self.sentencizer_func(str)
-        print(f"Return sentencizer {type(str)}")
         str = self.summarizer_func(str)
-        print(f"Return summarizer {type(str)}")
         str = self.filter_text(index, str)
-        print(f"Return filter_text {type(str)}")
-#         Return format_txt <class 'str'>
-# Sentencizer <class 'str'>
-# Return sentencizer <class 'list'>
-# Return summarizer <class 'list'>
-# Filter <class 'list'>
-# Return filter_text <class 'list'>
         # str = self.sentencizer.format_text(self.reviews['text'][index])
         # str = self.sentencizer.sents(str)
         # str = self.summarizer.summarize(str)
